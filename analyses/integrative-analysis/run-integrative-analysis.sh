e91553613c35a3ba4d02bb0078658220b2c7a8e6
AntoniaChroni <achroni@noderome162.cm.cluster>
2025-07-31 04:12:45 -0500

    Rm code for modules

--- analyses/integrative-analysis/run-integrative-analysis.sh
+++ analyses/integrative-analysis/run-integrative-analysis.sh
@@ -6,14 +6,6 @@ set -o pipefail
 # set up running directory
 cd "$(dirname "${BASH_SOURCE[0]}")" 
 
-# Set up modules
-#module load R/4.4.0
-#module load pandoc/1.19.2.1
-#module load pandoc/3.2
-#module load texlive/20240326 #to load LaTeX on a compute node
-
-#module load python/3.9.9
-
 ################################################################################################################
 # Run module
 Rscript --vanilla run-integrative-analysis.R
